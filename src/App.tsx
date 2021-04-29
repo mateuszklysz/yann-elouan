8f6cac1daeaec81826f5c231a0112cde1cb3a848
Mateusz Kłysz <mateuszklysz@outlook.com>
2021-04-29 11:48:41 +0200

    change files structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import "./styles/App/style.scss";
+import "./App.scss";
 import Hero from "./components/Hero/Hero";
 import Loader from "./components/Loader/Loader";
 import ILoadedContext from "./ts/loaded";
